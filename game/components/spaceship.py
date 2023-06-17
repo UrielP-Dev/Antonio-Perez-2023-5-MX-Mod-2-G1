d5a14e37404e571d531b095ef4120569083513f5
Uriel-PP <kaattoo9@gmail.com>
2023-06-16 18:03:03 -0600

    Tarea 1 - Inicio

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -10,10 +10,21 @@ class Spaceship(Sprite):
         self.image = SPACESHIP
         self.image = pygame.transform.scale(self.image,(self.image_width, self.image_heigth))
         self.rect = self.image.get_rect()
+        
     
     def draw(self, screen):
         screen.blit(self.image,(self.rect.x, self.rect.y))
         
           
-    def update(self, keybord_events):    
-        pass
+    def update(self, keyboard_events):    
+        speed = 5  # Velocidad de movimiento de la nave
+
+        # Verificar las teclas presionadas y actualizar la posición de la nave
+        if keyboard_events[pygame.K_LEFT]:
+            self.rect.x -= speed
+        elif keyboard_events[pygame.K_RIGHT]:
+            self.rect.x += speed
+        elif keyboard_events[pygame.K_UP]:
+            self.rect.y -= speed
+        elif keyboard_events[pygame.K_DOWN]:
+            self.rect.y += speed
