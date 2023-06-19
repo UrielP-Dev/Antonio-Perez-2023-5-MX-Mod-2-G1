c3567d13095dd7b29869bf811e5070f63a445431
Uriel-PP <kaattoo9@gmail.com>
2023-06-19 01:02:50 -0600

    Tarea 2 - Enemy

--- game/components/enemy.py
+++ game/components/enemy.py
@@ -1,30 +1,34 @@
 import pygame
 from pygame.sprite import Sprite
+import random
 
+# Gener
 from game.utils.constants import ENEMY_1 ,SCREEN_WIDTH, SCREEN_HEIGHT
 
-class Enemy(Sprite):
-    def __init__(self,):
-        self.image_width =  40
-        self.image_heigth = 50
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image_width = 40
+        self.image_height = 50
         self.image = ENEMY_1
-        self.image = pygame.transform.scale(self.image,(self.image_width, self.image_heigth))
+        self.image = pygame.transform.scale(self.image, (self.image_width, self.image_height))
         self.rect = self.image.get_rect()
-        self.rect.centerx = SCREEN_WIDTH // 2
-         
+        self.rect.centerx = random.randint(50, SCREEN_WIDTH - self.image_width)
+        
 
     def draw(self, screen):
-        screen.blit(self.image,(self.rect.x, self.rect.y))    
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def move(self):
+        self.rect.y += 8
+        if self.rect.y > SCREEN_HEIGHT:
+            self.rect.y = 0
+            self.rect.centerx = random.randint(50, SCREEN_WIDTH - self.image_width)
 
     def update(self):
-        print(self.rect.x)
-        flagR = True
-        flagL = False
-        if flagR and self.rect.x < SCREEN_WIDTH :
-            self.rect.x += 5
-        elif self.rect.x > SCREEN_WIDTH:
-            flagR = False
-            self.rect.x -= 5
+        self.move()
+        
+        
 
         
             
--- game/components/game.py
+++ game/components/game.py
@@ -1,6 +1,7 @@
 import pygame
+import random
 
-from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
+from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from game.components.spaceship import Spaceship
 from game.components.enemy import Enemy
 
@@ -12,51 +13,55 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
-        self.game_speed = 10 # el numero de pixeles que el "objeto / imagen" se mueve en patalla
+        self.game_speed = 10
         self.x_pos_bg = 0
         self.y_pos_bg = 0
         self.spaceship = Spaceship(SCREEN_WIDTH, SCREEN_HEIGHT)
-        self.enemy = Enemy()
+        self.enemies = []
+        self.create_enemies()
+
+    def create_enemies(self):
+        num_enemies = 6  # Número de enemigos a crear
+        for _ in range(num_enemies):
+            enemy = Enemy()
+            self.enemies.append(enemy)
 
     def run(self):
-        # Game loop: events - update - draw
         self.playing = True
         while self.playing:
             self.handle_events()
             self.update()
             self.draw()
         else:
-            print("Something ocurred to quit the game!")
+            print("Something occurred to quit the game!")
         pygame.display.quit()
         pygame.quit()
 
     def handle_events(self):
-        # pygame.event.get() es un iterable (lista)
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: #el QUIT event es el click en el icono que cierra ventana
+            if event.type == pygame.QUIT:
                 self.playing = False
 
     def update(self):
         events = pygame.key.get_pressed()
         self.spaceship.update(events)
-        self.enemy.update()
-        
-       
+        for enemy in self.enemies:
+            enemy.update()
 
     def draw(self):
-        self.clock.tick(FPS) # configuro cuantos frames per second voy a dibujar
-        self.screen.fill((255, 255, 255)) # lleno el screen de color BLANCO???? 255, 255, 255 es el codigo RGB
+        self.clock.tick(FPS)
+        self.screen.fill((255, 255, 255))
         self.draw_background()
         self.spaceship.draw(self.screen)
-        self.enemy.draw(self.screen)
-        
-        pygame.display.update() # esto hace que el dibujo se actualice en el display de pygame
-        pygame.display.flip()  # hace el cambio
+        for enemy in self.enemies:
+            enemy.draw(self.screen)
+        pygame.display.update()
+        pygame.display.flip()
 
     def draw_background(self):
         image = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        image_height = image.get_height()# alto de la imagen
-        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg)) # blit "dibuja"
+        image_height = image.get_height()
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg))
         self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
         if self.y_pos_bg >= SCREEN_HEIGHT:
             self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
