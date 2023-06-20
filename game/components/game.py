ecf2fb16b9ffdb7a1bfa77a6a540c61b2772fbbd
Uriel-PP <kaattoo9@gmail.com>
2023-06-20 15:07:24 -0600

    Tarea 2 Corregido

--- game/components/game.py
+++ game/components/game.py
@@ -1,9 +1,8 @@
 import pygame
-import random
 
-from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
 from game.components.spaceship import Spaceship
-from game.components.enemy import Enemy
+from game.components.enemies.enemy_handler import EnemyHandler
 
 class Game:
     def __init__(self):
@@ -13,55 +12,49 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
-        self.game_speed = 10
+        self.game_speed = 10 # el numero de pixeles que el "objeto / imagen" se mueve en patalla
         self.x_pos_bg = 0
         self.y_pos_bg = 0
-        self.spaceship = Spaceship(SCREEN_WIDTH, SCREEN_HEIGHT)
-        self.enemies = []
-        self.create_enemies()
-
-    def create_enemies(self):
-        num_enemies = 6  # Número de enemigos a crear
-        for _ in range(num_enemies):
-            enemy = Enemy()
-            self.enemies.append(enemy)
+        self.player = Spaceship()
+        self.enemy_handler = EnemyHandler()
 
     def run(self):
+        # Game loop: events - update - draw
         self.playing = True
         while self.playing:
             self.handle_events()
             self.update()
             self.draw()
         else:
-            print("Something occurred to quit the game!")
+            print("Something ocurred to quit the game!")
         pygame.display.quit()
         pygame.quit()
 
     def handle_events(self):
+        # pygame.event.get() es un iterable (lista)
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: #el QUIT event es el click en el icono que cierra ventana
                 self.playing = False
 
     def update(self):
-        events = pygame.key.get_pressed()
-        self.spaceship.update(events)
-        for enemy in self.enemies:
-            enemy.update()
+        # pass # pass equivale a hacer nada 
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
+        self.enemy_handler.update()
 
     def draw(self):
-        self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255))
         self.draw_background()
-        self.spaceship.draw(self.screen)
-        for enemy in self.enemies:
-            enemy.draw(self.screen)
-        pygame.display.update()
-        pygame.display.flip()
+        self.clock.tick(FPS) # configuro cuantos frames per second voy a dibujar
+        self.player.draw(self.screen)
+        self.enemy_handler.draw(self.screen)
+        # self.screen.fill((255, 255, 255)) # lleno el screen de color BLANCO???? 255, 255, 255 es el codigo RGB
+        pygame.display.update() # esto hace que el dibujo se actualice en el display de pygame
+        pygame.display.flip()  # hace el cambio
 
     def draw_background(self):
         image = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        image_height = image.get_height()
-        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg))
+        image_height = image.get_height()# alto de la imagen
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg)) # blit "dibuja"
         self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
         if self.y_pos_bg >= SCREEN_HEIGHT:
             self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,30 +1,52 @@
 import pygame
-from pygame.sprite import Sprite
+from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT, DEFAULT_TYPE
 
-from game.utils.constants import SPACESHIP,SCREEN_WIDTH, SCREEN_HEIGHT
+class Spaceship:
+    X_POS = (SCREEN_WIDTH // 2) - 40
+    Y_POS = 500
+    SPACESHIP_WIDTH = 40
+    SPACESHIP_HEIGHT = 60
+    SPACESHIP_SPEED = 10
 
-class Spaceship(Sprite):
-    def __init__(self,screen_width, screen_height):
-        self.image_width =  40
-        self.image_heigth = 50
+    def __init__(self):
         self.image = SPACESHIP
-        self.image = pygame.transform.scale(self.image,(self.image_width, self.image_heigth))
+        self.image = pygame.transform.scale(self.image, (self.SPACESHIP_WIDTH, self.SPACESHIP_HEIGHT))
         self.rect = self.image.get_rect()
-        self.rect.centerx = SCREEN_WIDTH // 2
-        self.rect.centery = SCREEN_HEIGHT // 2
-        self.speed = 15
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
+        self.is_alive = True
+
+    def update(self, user_input):
+        if user_input[pygame.K_LEFT]:
+            self.move_left()
         
+        if user_input[pygame.K_RIGHT]:
+            self.move_right()
+        
+        if user_input[pygame.K_UP]:
+            self.move_up()
+        
+        if user_input[pygame.K_DOWN]:
+            self.move_down()
+
     def draw(self, screen):
-        screen.blit(self.image,(self.rect.x, self.rect.y))
-        
-          
-    def update(self, keyboard_events):    
-        
-        if keyboard_events[pygame.K_LEFT] and  self.rect.x > 5 :
-            self.rect.x -= self.speed
-        elif keyboard_events[pygame.K_RIGHT] and self.rect.x < SCREEN_WIDTH - self.image_width: 
-            self.rect.x += self.speed
-        elif keyboard_events[pygame.K_UP] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        elif keyboard_events[pygame.K_DOWN] and self.rect.y < SCREEN_HEIGHT - self.image_heigth:
-            self.rect.y += self.speed
+        screen.blit(self.image, self.rect)
+
+    def move_left(self):
+        self.rect.x -= self.SPACESHIP_SPEED
+        if self.rect.left < 0 - self.SPACESHIP_WIDTH:
+            self.rect.x = SCREEN_WIDTH
+            
+    def move_right(self):
+        self.rect.x += self.SPACESHIP_SPEED
+        if self.rect.right > SCREEN_WIDTH + self.SPACESHIP_WIDTH:
+            self.rect.x = 0
+
+    def move_up(self):
+        if self.rect.y > SCREEN_HEIGHT // 2:
+            self.rect.y -= self.SPACESHIP_SPEED
+    
+    def move_down(self):
+        if self.rect.y < SCREEN_HEIGHT - 70:
+            self.rect.y += self.SPACESHIP_SPEED
+
